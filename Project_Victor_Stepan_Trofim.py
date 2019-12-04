e0c73c7c900db25db1385dcdbe4dd912a44b86f9
ssyrovatkin <Stepashan@bk.ru>
2019-12-04 18:36:19 +0300

    add shooting

--- Project_Victor_Stepan_Trofim.py
+++ Project_Victor_Stepan_Trofim.py
@@ -15,15 +15,49 @@ def create_objects():
     canv.create_oval (100, 25, 700, 625, outline="gray", fill="red", width=2)
     a = 5
     canv.create_oval (400-a, 325-a, 400+a, 325+a, fill="blue", width=2)
-    
-    
+
+
+class Ball():
+    def __init__(self, x=400, y=325):
+        
+        self.x = x
+        self.y = y
+        self.r = 2
+        self.vx = 40
+        self.vy = 40
+        self.color = choice(['blue', 'green', 'red', 'brown'])
+        self.id = canv.create_oval(
+                self.x - self.r,
+                self.y - self.r,
+                self.x + self.r,
+                self.y + self.r,
+                fill=self.color
+        )
+
+    def set_coords(self):
+        canv.coords(
+                self.id,
+                self.x - self.r,
+                self.y - self.r,
+                self.x + self.r,
+                self.y + self.r
+        )
+        
+
+    def move(self):
+        self.x += self.vx
+        self.y -= self.vy
+        self.set_coords()    
+
+
 class Player():
     
     def __init__(self, ):
-        
+        self.f2_power = 20
         self.f2_on = 0
         self.x = 400
         self.y = 325
+        self.an = 1
         #self.color = choice(['blue', 'green', 'yellow', 'brown'])
         self.a1 = 30
         self.a2 = 15
@@ -38,7 +72,7 @@ class Player():
                 fill='blue'
         )
         
-        self.id3 = canv.create_line(self.x, self.y ,self.x, self.y - self.l, width=5)
+        self.id3 = canv.create_line(self.x, self.y ,self.x, self.y - self.l, width=6)
         
         self.id2 = canv.create_oval(
                 self.x - self.a2,
@@ -75,11 +109,29 @@ class Player():
             self.x,
             self.y - self.l
         )
-    
+
+
+    def fire2_start(self, event):
+        self.f2_on = 1
+
+
+    def fire2_end(self, event):
+        global balls, bullet
+        bullet += 1
+        new_ball = Ball(self.x, self.y)
+        new_ball.r += 5
+        self.an = math.atan2((event.y-new_ball.y) , (event.x-new_ball.x))
+        new_ball.vx = self.f2_power * math.cos(self.an)
+        new_ball.vy = - self.f2_power * math.sin(self.an)
+        balls += [new_ball]
+        self.f2_on = 0
+        self.f2_power = 20
+        
+        
     def targetting(self, event=0):
         """Прицеливание. Зависит от положения мыши."""
         if event:
-            self.an = math.atan2((event.y-self.y) , (event.x-self.x))
+            self.an = math.atan2((event.y-self.y), (event.x-self.x))
         canv.coords(self.id3, 
                     self.x, 
                     self.y,
@@ -112,15 +164,24 @@ class Player():
         self.set_coords1()
         self.set_coords2()
         self.set_coords3()
-        
+
+balls = []
+bullet = 0
+
 def game_process(event=''):
-    
+    global balls, bullet
+    root.bind('<Button-1>', P1.fire2_start)
+    root.bind('<ButtonRelease-1>', P1.fire2_end)
     root.bind('<Motion>', P1.targetting)
     root.bind('<Right>', P1.move_right)    
     root.bind('<Left>', P1.move_left)
     root.bind('<Up>', P1.move_up)
     root.bind('<Down>', P1.move_down)
-    
+    for b in balls:
+            b.move()
+    canv.update()
+    time.sleep(0.03)
+    P1.targetting()
     root.after(750, game_process)
     
 create_objects()
