2ceabb3162475d89e0c6d40de3a569f8af4a1667
ssyrovatkin <Stepashan@bk.ru>
2019-12-09 22:33:56 +0300

    add scores

--- Project_Victor_Stepan_Trofim.py
+++ Project_Victor_Stepan_Trofim.py
@@ -10,26 +10,25 @@ fr = tk.Frame(root)
 root.geometry('800x650')
 canv = tk.Canvas(root, bg='pink')
 canv.pack(fill=tk.BOTH, expand=1)
+all_points = 0
+id_points = canv.create_text(30,30,text = all_points,font = '28')
+
+
 k_x = 1
 k_y = 1
-
 i=0
 k=0
 x_0 = 100
 y_0 = 25
 R = 300
 a = 200
-enemys=[]
-
+screen1 = canv.create_text(400, 300, text='', font='28')
 M = 1
 dt = 1
 w = 0.001
-<<<<<<< HEAD
 yc = 325
 xc = 500
-=======
->>>>>>> 4b2695f7cad16bd4aa6e97318d371ca8084a44b3
-
+enemys=[]
 
 
 def create_objects():
@@ -43,11 +42,7 @@ class Ball():
         self.x = x
         self.y = y
         self.r = 2
-<<<<<<< HEAD
-        self.live = 20
-=======
-        self.live = 200
->>>>>>> 4b2695f7cad16bd4aa6e97318d371ca8084a44b3
+        self.live = 25
         self.vx = 40
         self.vy = 40
         self.color = choice(['blue', 'green', 'red', 'brown'])
@@ -75,19 +70,6 @@ class Ball():
         self.set_coords()    
 
 
-    def hittest(self, obj):
-        """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
-
-        Args:
-            obj: Обьект, с которым проверяется столкновение.
-        Returns:
-            Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
-        """
-        if ((self.x-obj.x)**2+(self.y-obj.y)**2)**0.5 <= self.r + obj.r:
-            return True
-        else:
-            return False 
-
 class Player():
     
     def __init__(self, ):
@@ -95,11 +77,7 @@ class Player():
         self.f2_on = 0
         self.x = 400
         self.y = 325
-<<<<<<< HEAD
         self.live = 100
-=======
-        
->>>>>>> 4b2695f7cad16bd4aa6e97318d371ca8084a44b3
         
         self.ay = 0
         self.ax = 0
@@ -171,7 +149,6 @@ class Player():
 
     def fire2_end(self, event):
         global balls, bullet
-        bullet += 1
         new_ball = Ball(self.x, self.y)
         new_ball.r += 5
         self.an = math.atan2((event.y-new_ball.y) , (event.x-new_ball.x))
@@ -221,13 +198,8 @@ class Player():
         self.set_coords3()
     def move_right(self, event):  
         global k_x, k_y
-<<<<<<< HEAD
         self.vx+=-self.F*k_x/self.m
         self.vy+=self.F*k_y/self.m
-=======
-        self.x += -k_y*self.v
-        self.y += k_x*self.v
->>>>>>> 4b2695f7cad16bd4aa6e97318d371ca8084a44b3
         self.set_coords1()
         self.set_coords2()
         self.set_coords3()
@@ -235,13 +207,8 @@ class Player():
         
     def move_left(self, event):
         global k_x, k_y
-<<<<<<< HEAD
         self.vx+=self.F*k_x/self.m
         self.vy+=-self.F*k_y/self.m
-=======
-        self.x += k_y*self.v
-        self.y += -k_x*self.v
->>>>>>> 4b2695f7cad16bd4aa6e97318d371ca8084a44b3
         self.set_coords1()
         self.set_coords2()
         self.set_coords3()
@@ -259,13 +226,8 @@ class Player():
     def move_down(self, event):
         global k_x, k_y
         
-<<<<<<< HEAD
         self.vx+=-self.F*k_x/self.m
         self.vy+=-self.F*k_y/self.m
-=======
-        self.x += -k_x*self.v
-        self.y += -k_y*self.v
->>>>>>> 4b2695f7cad16bd4aa6e97318d371ca8084a44b3
         self.set_coords1()
         self.set_coords2()
         self.set_coords3()
@@ -333,18 +295,23 @@ def change_velocity_vy(vx, vy, x, y):
     return vy
 
 
+def hit(all_points, id_points):
+    canv.coords(self.id, -10, -10, -10, -10)
+    all_points += points
+    canv.itemconfig(id_points, text=all_points)
+
+
 def new_enemy():
     x = rnd(100,700)
     y = rnd(100,500)
     r = rnd(10,15)
     vx=rnd(-6, 6)
     vy=rnd(-6, 6)
-    live=1
     if (x - (x_0 + R))**2 + (y - (y_0 + R))**2 >= a**2 and (x - (x_0 + R))**2 + (y - (y_0 + R))**2 <= (R-40)**2 :
         id_ = canv.create_oval( x - r, y - r, x + r, y + r,fill = 'black', width=0)
-        enemy={'id': id_, 'x': x, 'y': y, 'r': r, 'vx': vx, 'vy': vy, 'live': live}
+        enemy={'id': id_, 'x': x, 'y': y, 'r': r, 'vx': vx, 'vy': vy}
         enemys.append(enemy)
-    root.after(300,new_enemy)
+    root.after(200,new_enemy)
 
 def motion():
     for e in enemys:
@@ -363,7 +330,8 @@ balls = []
 bullet = 0
 
 def game_process(event=''):
-    global balls, bullet
+    global balls, bullet, all_points
+    canv.itemconfig(screen1, text=str(bullet))
     root.bind('<Button-1>', P1.fire2_start)
     root.bind('<ButtonRelease-1>', P1.fire2_end)
     root.bind('<Motion>', P1.targetting)
@@ -373,23 +341,17 @@ def game_process(event=''):
     root.bind('<Down>', P1.move_down)
     delete = []
     for b in balls:
-<<<<<<< HEAD
         b.move()
         for k, e in enumerate(enemys):   
             if (b.x-e['x'])**2 + (b.y-e['y'])**2 <= (b.r + e['r'])**2:
                 canv.delete(e['id'])
+                all_points +=1
                 del enemys[k]
         if b.live<=0:
             canv.delete(b.id)
         b.live+= -1
 
-        
-=======
-            b.move()
-            b.live+= -1
-            if b.live<=0:
-                canv.delete(b.id)
->>>>>>> 4b2695f7cad16bd4aa6e97318d371ca8084a44b3
+    canv.itemconfig(id_points, text='Score:'+str(all_points))
     canv.update()
     P1.move()
     P1.acceleration()
